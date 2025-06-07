f4f968cda6391a30de6e698294f4c707317981fc
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-06-07 05:45:31 -0300

    admin_user

--- admin_user.py
+++ admin_user.py
@@ -1,2 +1,22 @@
 def admin_user(user):
-    pass
+    
+    while True:
+        print(f"----------------- Bienvenido {user["nombre"]} -----------------")
+        print("Administrador")
+        print("\n")
+        print("1 Consultar automatizaciones activas")
+        print("2 Gestionar dispositivos")
+        print("3 Modificar el rol de un usuario")
+        print("4 Salir de la sesion \n")
+                
+        opcion=int(input("ingrese una opcion: "))
+        
+        if opcion == 1:
+            pass 
+        if opcion == 2:
+            pass 
+        if opcion == 3:
+            pass
+            
+        if opcion == 4:
+            break
