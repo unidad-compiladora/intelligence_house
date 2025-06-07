5a576880a01db09cf88a1af8785542165c9326c7
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-06-07 03:55:51 -0300

    creacion archivo user_data_base

---
+++ users_data_base.py
@@ -0,0 +1,33 @@
+usuarios=[
+    {   
+        "nombre": "Carlos",
+        "apellido": "Gómez",
+        "email": "carlos.gomez@example.com",
+        "contrasena": "password123",
+        "es_admin": True
+    },
+    {   
+
+        "nombre": "Lucía",
+        "apellido": "Martínez",
+        "email": "lucia.martinez@example.com",
+        "contrasena": "lucia456",
+        "es_admin": False
+    },
+    {   
+        "nombre": "Diego",
+        "apellido": "Fernández",
+        "email": "diego.fernandez@example.com",
+        "contrasena": "diego789",
+        "es_admin": False
+    },
+    {   
+        "nombre": "María",
+        "apellido": "López",
+        "email": "maria.lopez@example.com",
+        "contrasena": "maria321",
+        "es_admin": False
+    }
+]
+
+
