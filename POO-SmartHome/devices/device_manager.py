d41d40ab9a2c15671a0e3112e59652d9d6a27f3a
nahuel bocanegra <nahuboca7@gmail.com>
2025-10-20 19:31:21 +0200

    WIP: avance parcial en CRUD dispositivos

--- POO-SmartHome/devices/device_manager.py
+++ POO-SmartHome/devices/device_manager.py
@@ -1,5 +1,5 @@
 #necesito hacer la conexion con db para traer los dispositivos
-dispositivos=None
+devices=None
 
 
 
@@ -7,10 +7,10 @@ class Divice_manager():
 
     def list_divices(self):
 
-        if dispositivos:
+        if devices:
             print("Dispositivos disponibles")
 
-            for nombre in dispositivos.keys():
+            for nombre in devices.keys():
                 print(f"  - {nombre} ")
         else:
 
@@ -20,10 +20,19 @@ class Divice_manager():
             
         self.list_divices()
 
-        if nombre_dispositivo in dispositivos:
+        if nombre_dispositivo in devices:
        
             return f"el dispositivos{nombre_dispositivo} fue encontrado"
         else:
             return f"el dispositivos{nombre_dispositivo} no fue encontrado"
     
-   
+    def add_devices(self,new_devices):
+       
+        if  new_devices in devices:
+            return "ya se encuentra este dispositivo "
+        else:
+            devices[new_devices]={"datos":"dato"}
+
+        self.list_divices()
+
+    
