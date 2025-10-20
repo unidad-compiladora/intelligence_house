893a44d088c7840d17eb8900a207dd6b679d19f2
nahuel bocanegra <nahuboca7@gmail.com>
2025-10-20 20:53:55 +0200

    metodo: connect

--- POO-SmartHome/db/connection.py
+++ POO-SmartHome/db/connection.py
@@ -9,6 +9,20 @@ class DataBase:
         self.user=user,
         self.password=password,
         self.database=database
+        self.con=None
 
+    def connect(self):
+
+        try: 
+            self.conn=mysql.connector.connect(
+        
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database
+
+            )
+        except Error as e:
+            print(f"Error de conexion: {e}")
  
 
