2f9dde9a599e3b974d8b747e25c7a2f89e26fa23
nahuel bocanegra <nahuboca7@gmail.com>
2025-10-28 18:59:21 +0100

    devices modificaciones

--- POO-SmartHome/devices/device_manager.py
+++ POO-SmartHome/devices/device_manager.py
@@ -1,72 +1,110 @@
-from dao.interfaces.devices_dao import Devices_dao
-
-#necesito hacer la conexion con db para traer los dispositivos
-devices=None
+#from dao.interfaces.devices_dao import Devices_dao
+from db.connection import DataBase
 
 
 
-class Device_manager(Devices_dao):
+class Device_manager():
 
-    def list_devices(self):
+    db=DataBase()
 
-        if devices:
-            print("Dispositivos disponibles")
+    def __init__(self):
+        self.conn=self.db.connect()
 
-            for name in devices.keys():
-                print(f"  - {name} ")
-        else:
 
-            print("No hay dispositivos disponibles")
+    def get_by_id(self, id):
+        
+        cursor=self.conn.cursor()
+        query="SELECT * FROM Devices WHERE device_id = %s"
+        cursor.execute(query,(id,))
+        device=cursor.fetchone()        
 
-    def search_devices(self,nombre_dispositivo):
-            
-        self.list_devices()
+        if device is not None:   
+            print(f"device:{device[2]}")
+        else: 
+            print("este dispositivo no esta en nuestra base de datos")
 
-        if nombre_dispositivo in devices:
-       
-            return f"el dispositivos{nombre_dispositivo} fue encontrado"
-        else:
-            return f"el dispositivos{nombre_dispositivo} no fue encontrado"
+        cursor.close()
+        
+
+    def get_all(self):
+
+
+
+        cursor=self.conn.cursor()
+        cursor.execute("SELECT * FROM Devices")
+        resultado = cursor.fetchall()
+
+        if device is not None:
+            for device in resultado:
+                print(f"{device[0]}device:{ device[2]}")
+        else: 
+            print("vacio")
+
+        cursor.close()
+
     
-    def add_devices(self,new_devices):
+    def update(self, id, data):
+
+        pass
+    
+
+    def delete_by_id(self, id):
+
+        cursor=self.conn.cursor()
+        query="DELETE FROM Devices WHERE device_id = %s"
+        cursor.execute(query,(id,))
+
+        self.conn.commit()
+
+        cursor.close()
+    
+    db.close()
+
+
+   
+    # def add_devices(self,new_devices):
        
-        if  new_devices in devices:
-            return "ya se encuentra este dispositivo "
-        else:
-            devices[new_devices]={"datos":"dato"}
+    #     if  new_devices in devices:
+    #         return "ya se encuentra este dispositivo "
+    #     else:
+    #         devices[new_devices]={"datos":"dato"}
 
-        self.list_devices()
+    #     self.list_devices()
 
-    def delete_devices(self,device_name):
+    # def delete_devices(self,device_name):
      
-        if device_name in devices:
-            del devices[device_name]
-            print(f"Dispositivo '{device_name}' eliminado exitosamente.")
-        else:
-            print(f"El dispositivo '{device_name}' no fue encontrado.")
+    #     if device_name in devices:
+    #         del devices[device_name]
+    #         print(f"Dispositivo '{device_name}' eliminado exitosamente.")
+    #     else:
+    #         print(f"El dispositivo '{device_name}' no fue encontrado.")
     
-        self.list_devices()
+    #     self.list_devices()
 
-    def set_status_devices(self,name,new_state=False):
-        if name in devices:
-            devices[name]["estado"] = new_state
-            print(f"'{name}' ahora está {'encendido' if new_state else 'apagado'}.")
-        else:
-            print(f"Dispositivo '{name}' no encontrado.")
+    # def set_status_devices(self,name,new_state=False):
+    #     if name in devices:
+    #         devices[name]["estado"] = new_state
+    #         print(f"'{name}' ahora está {'encendido' if new_state else 'apagado'}.")
+    #     else:
+    #         print(f"Dispositivo '{name}' no encontrado.")
 
     
     
-    def update_devices(self,name,**kwargs):
-        if name in devices:
-            devices[name].update(kwargs)
-            print(f"'{name}' actualizado con exito")
-        else:
-            print(f"Dispositivo '{name}' no encontrado.")
+    # def update_devices(self,name,**kwargs):
+    #     if name in devices:
+    #         devices[name].update(kwargs)
+    #         print(f"'{name}' actualizado con exito")
+    #     else:
+    #         print(f"Dispositivo '{name}' no encontrado.")
 
-    def get_status_devices(name):
-        if name in devices:
-            print(f"Estado de '{name}':")
-            for atribute, value in devices[name].items():
-                print(f"  {atribute}: {value}")
-        else:
-            print(f"Dispositivo '{name}' no encontrado.")
+    # def get_status_devices(name):
+    #     if name in devices:
+    #         print(f"Estado de '{name}':")
+    #         for atribute, value in devices[name].items():
+    #             print(f"  {atribute}: {value}")
+    #     else:
+    #         print(f"Dispositivo '{name}' no encontrado.")
+
+
+device=Device_manager()
+device.get_by_id(1)
