f6b194f471133eb6e63b2ae7893ecfd6c6dd45d3
nahuel bocanegra <nahuboca7@gmail.com>
2025-10-29 16:35:13 +0100

    users_dao

---
+++ SmartHome/src/dao/interfaces/users_dao.py
@@ -0,0 +1,27 @@
+from abc import ABC , abstractmethod
+
+class Users_dao(ABC):
+    
+    @abstractmethod
+    def get_all(self):
+        
+        pass
+
+    @abstractmethod
+    def get_by_id(self,id:int):
+        
+        pass
+
+    @abstractmethod
+    def delete_by_id(self,id:int):
+    
+        pass
+
+    @abstractmethod
+    def update(self,id:int,data:dict):
+        
+        pass
+
+    @abstractmethod
+    def post(self,data:dict):
+        pass
